1d405761fd3d731cebb8a11ae2c7f0d7a9a414eb
Matt Pudig <mp2135@gadi-login-07.gadi.nci.org.au>
2021-08-04 18:28:33 +1000

    Updates to one_dim_experiments. Can now run many many experiments to optimise paramaters.

--- one_dim_model.py
+++ one_dim_model.py
@@ -2,28 +2,76 @@ import numpy as np
 
 # Constants, data, parameters...
 
-rho = 1025.0 # units: kg/m^3
-c = 4000.0 # units: J/(kg C)
+rho = 1025 # kg/m^3
+c = 4000 # J/(kg C)
+
+z_m = 50.0 # Depth of minimum mixing layer
+z_d = 900.0 # Depth for deep ocean restoring term
+z_b = 1000.0 # Depth of thermocline
+
+mu = 1/(20 * 365 * 86400) # Deep ocean restoring time scale
+gamma = 1/(1.5 * 365 * 86400) # Surface restoring time scale (to be determined through my least squares regression)
+kappa = 7e-6
+
+T_0 = 22.0 # Top temperature for initial condition
+T_b = 5.0 # Bottom temperature for initial condition (this warms up a bit during runs because of the no-flux at bottom condition)
 
 dt = 10.0 * 86400 # units: s
 dz = 5.0 # units: m
 
-T_top = 24.0 # Top temperature
-T_bot = 6.0 # Bottom temperature
-
-z_mix = 50.0 # Depth of minimum mixing layer
-z_base = 1000.0 # Depth of thermocline
-
+z = np.linspace(0.0, z_b, int(z_b / dz) + 1) # Depth at top/bottom of grid cell
+z_t = (z[1:] + z[:-1]) / 2 # Depth at the middle of grid cell
 
 
 ### Intermediate functions
 
-def steady_state(z):
-    mixing_index = np.where(z_mix - z == 0)[0][0]
-    mixing_layer = T_top * np.ones(mixing_index)
-    z = z[mixing_index + 1:]
-    thermocline = ((T_top - T_bot) * z - (T_top * z_base - T_bot * z_mix)) / (z_mix - z_base)
-    return np.concatenate((mixing_layer, thermocline), axis=None)
+# Initial condition: constant in mixing layer, linearly stratified elsewhere
+
+def initial_condition(z_t):
+    mixing_index = (np.abs(z_t - z_m)).argmin() # Index in z_t exactly dz/2 m above z_m.
+    mixing_layer = T_0 * np.ones(mixing_index + 1)
+    z_t = z_t[mixing_index + 1:]
+    thermocline = ((T_0 - T_b) * z_t - (T_0 * z_b - T_b * z_m)) / (z_m - z_b)
+    return np.concatenate((mixing_layer, thermocline), axis = None)
+
+# Analytical steady state, cf. Appendix 1
+
+def steady_state(Z):
+    
+    # Paramaters
+    A = (1 + np.exp(2 * np.sqrt(mu / kappa) * (z_b - z_d))) * np.exp(np.sqrt(mu / kappa) * z_d)
+    
+    B = (T_0 - T_b) / (z_m - z_b) * (z_m - z_d - np.sqrt(kappa / mu) * np.exp(np.sqrt(mu / kappa) * (z_b - z_d)))
+    
+    C = np.sqrt(gamma / kappa) * (np.exp(z_m * np.sqrt(gamma / kappa)) - np.exp( - z_m * np.sqrt(gamma / kappa))) * (z_d - z_m) \
+    + np.exp(z_m * np.sqrt(gamma / kappa)) + np.exp( - z_m * np.sqrt(gamma / kappa))
+    
+    D = np.sqrt(gamma / kappa) * (np.exp(z_m * np.sqrt(gamma / kappa)) - np.exp( - z_m * np.sqrt(gamma / kappa)))
+    
+    E = (T_0 - T_b) / (z_m - z_b) * (1 - np.exp((z_b - z_d) * np.sqrt(mu / kappa)))
+    
+    F = np.sqrt(mu / kappa) * (1 - np.exp(2 * (z_b - z_d) * np.sqrt(mu / kappa))) * np.exp(z_d * np.sqrt(mu / kappa))
+    
+    # Coefficients
+    a_1 = (A * E + B * F) / (A * D - C * F)
+    
+    c_1 = (C * E + B * D) / (A * D - C * F)
+    
+    # Piecewise defined function (ie, need to use vectorise):
+    if Z <= z_m:
+        
+        return T_0 + a_1 * (np.exp(Z * np.sqrt(gamma / kappa)) + np.exp( - Z * np.sqrt(gamma / kappa)))
+    
+    elif Z > z_m and Z < z_d:
+        
+        return T_0 + a_1 * (np.sqrt(gamma / kappa) * (np.exp(z_m * np.sqrt(gamma / kappa)) - np.exp( - z_m * np.sqrt(gamma / kappa))) * (Z - z_m) \
+                            + np.exp(z_m * np.sqrt(gamma / kappa)) + np.exp( - z_m * np.sqrt(gamma / kappa)))
+    
+    else:
+        
+        return ((T_0 - T_b) * Z + z_m * T_b - z_b * T_0) / (z_m - z_b) \
+    + c_1 * (1 + np.exp(2 * (z_b - Z) * np.sqrt(mu / kappa))) * np.exp(Z * np.sqrt(mu / kappa)) \
+    + np.sqrt(kappa / mu) * (T_0 - T_b) / (z_m - z_b) * np.exp((z_b - Z) * np.sqrt(mu / kappa))
 
 
 def convective_adjustment(T):
@@ -33,61 +81,69 @@ def convective_adjustment(T):
     def mixing_Rahmstorf(T):
         
         for i in range(len(T) - 1):
-            
+
             if T[i] < T[i + 1]: # if there is an instability
-                
+
                 j = 0
-                
+
                 # This loop mixes the instability downwards
-                
+
                 while T[i + j] < T[i + j + 1]:
-                    
+
                     T[i : i + j + 2] = np.mean(T[i : i + j + 2])
-                    
+
                     j += 1
-                    
+
                 break
+            
             
         return T
     
-    # Run the Rahmstorf mixing algorithm until the instability has been eliminated from the water column (i.e., from the mixing layer) 
+    # Run the Rahmstorf mixing algorithm until the instability has been eliminated from the water column (i.e., from the mixing layer)
     
-    for k in range(int(z_mix / dz)):
+    for k in range(int(z_m / dz)):
         mixing_Rahmstorf(T)
     return mixing_Rahmstorf(T)
 
 
+### Ocean heat content
+
 def OHC(T):
+    
     OHC = np.empty(T.shape[1])
     
     for i in range(len(OHC)):
-        OHC[i] = np.mean(T[:, i])
+        
+        #OHC[i] = rho * c * np.cumsum(T[: , i])[- 1] * dz # This is for ocean heat content
+        
+        OHC[i] = np.mean(T[:, i]) # This is for average temperature
+        
     return OHC
 
 
 
 ### The forward-in-time, central-in-space scheme with restoring and flux boundary conditions
 
-def FTCS(dt, dz, kappa, gamma, mu, z_mix, years, T_initial, Q, T0):
+def model(dt, dz, z_m, z_d, z_b, kappa, gamma, T_initial, Q, T0, years):
     
     # Data
     
-    radiative_forcing = Q / (c * rho * z_mix)
+    radiative_forcing = Q / (c * rho * z_m)
     
     days = dt / 86400 # Timestep in days
-    M = int(z_base / dz) # Number of spatial steps
+    M = int(z_b / dz) # Number of spatial steps evaluated at the top/bottom of the grid cell
     N = int(years * 360 / days) # Number of timesteps in days (taking 1 year = 360 days)
-    z = np.linspace(0.0, z_base, M + 1)
+    z = np.linspace(0.0, z_b, M + 1) # Depth at top/bottom of grid cell
+    z_t = (z[1:] + z[:-1]) / 2 # Depth at the middle of grid cell 
     t = np.linspace(0.0, years * 360, N + 1)
     
-    Hmix = np.heaviside(z_mix - z, 0)
-    z_mix_index = np.where(z_mix - z == 0)[0][0] # this is the index of z_mix in z
+    Hmix = np.heaviside(z_m - z, 0) # 1 above z = z_m, zero elsewhere
+    Hdeep = np.heaviside(z - z_d, 1) # 1 below z = z_d, zero elsewhere
     
+    # Temperature and flux matrices. Prescribe initial and boundary conditions
     
-    # Temperature and flux. Prescribe initial and boundary conditions
-    
-    T = np.zeros((M, N + 1))
-    F = np.zeros((M + 1, N + 1))
+    T = np.zeros((M, N + 1)) # i.e. evaluated at the middle of grid cell
+    F = np.zeros((M + 1, N + 1)) # i.e. evaluated at the top/bottom of grid cell
     
     T[:, 0] = T_initial # Initial condition
     #T[-1, :] = T_bot
@@ -105,10 +161,9 @@ def FTCS(dt, dz, kappa, gamma, mu, z_mix, years, T_initial, Q, T0):
              
             T[m, n + 1] = T[m, n] + dt / dz * (F[m, n] - F[m + 1, n]) \
             + dt * (radiative_forcing[n] - gamma * (T[m, n] - (T0[n] + T[m, 0]))) * Hmix[m]  \
-            - dt * mu * (T[m, n] - T[m, 0]) * np.heaviside(z - 900, 1)[m] 
-            
+            - dt * mu * (T[m, n] - T[m, 0]) * Hdeep[m] 
+            # NB: "+ w" since z is positive downwards, this is an upwelling velocity, implying that it would have been negative on the LHS
             #  + w * dt * (- F[m + 1, n] / kappa) \
-            # NB: "+ w" since z is positive downwards, this is an upwelling velocity, implying that it would have been negative on the LHS in order to balance diffusion
 
             
         #Convective adjustment step
